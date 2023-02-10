1d21bc5f42dd7b61435807f8ee6d04ee78d74183
Sergey Petrosyan <ipetriez@icloud.com>
2023-02-10 13:17:59 +0400

    Initial Commit

---
+++ UberCloneSwiftUIPetProject/UberCloneSwiftUIPetProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneSwiftUIPetProjectApp.swift
+//  UberCloneSwiftUIPetProject
+//
+//  Created by Sergey Petrosyan on 10.02.23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneSwiftUIPetProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
